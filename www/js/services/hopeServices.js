11c8ea52213ce44b9da390aa3f3ce1e5d0b5b069
cliffeby <clifford.eby@gmail.com>
2015-10-11 09:31:44 -0400

    Added Mongolab API

--- www/js/services/hopeServices.js
+++ www/js/services/hopeServices.js
@@ -1,6 +1,6 @@
 angular.module('mobilehope.controllers')
-.factory('mobileHopeDataService', ['$http','SERVER',function ($http,SERVER) {
-    var factory = {};
+    .factory('mobileHopeDataService', ['$http', 'SERVER', function ($http, SERVER) {
+        var factory = {};
 
         //factory.getCategoryData = function () {
         //    var categories = ([
@@ -21,46 +21,58 @@ angular.module('mobilehope.controllers')
         //    }, function(data){});
         //    return categories;
         //};
-        factory.getCategoryData = function() {
+        factory.getCategoryData = function () {
             var categories = [];
-                return $http({
-                        method: 'GET',
-                        url: SERVER.url + '/categories'+ SERVER.apiKey
-                    }).then(function(response){
-                            categories = response;
-                            console.log('Success: ',response, categories);
-                            return categories;
-                        })
-                };
+            return $http({
+                method: 'GET',
+                url: SERVER.url + '/categories' + SERVER.apiKey
+            }).then(function (response) {
+                categories = response;
+                console.log('Success: ', response, categories);
+                return categories;
+            })
+        };
 
-    factory.getItemData = function (id) {
-        var id = id;
-        var allItems = [];
-        return $http({
-            method: 'GET',
-            url: SERVER.url + '/allItems'+ SERVER.apiKey
-        }).then(function(response){
-            allItems = response;
-            console.log('Success: ',response, allItems);
-            return allItems;
-        })
-    };
+        factory.getItemData = function (id) {
+            var id = id;
+            var allItems = [];
+            return $http({
+                method: 'GET',
+                url: SERVER.url + '/allItems'  + SERVER.apiKey
+            }).then(function (response) {
+                allItems = response;
+                console.log('Success: ', response, allItems);
+                //    function filterByID(allItems) {
+                //        if (allItems.itemId == id) {
+                //            //console.log(id,'true');
+                //            return true;
+                //        } else {
+                //            //console.log(id,'false');
+                //            return false;
+                //        }
+                //    };
+                //    var items = allItems.filter(filterByID);
+                //    return items;
+                //};
+                return allItems;
+            })
+        };
 
-    //    function filterByID(allItems) {
-    //        if (allItems.itemId == id) {
-    //            //console.log(id,'true');
-    //            return true;
-    //        } else {
-    //            //console.log(id,'false');
-    //            return false;
-    //        }
-    //    };
-    //    var items = allItems.filter(filterByID);
-    //    return items;
-    //};
-    factory.getCategoryName = function (id) {
-        var name = this.getCategoryData()[id - 1].title;
-        return name;
-    };
-    return factory
-}]);
+        //    function filterByID(allItems) {
+        //        if (allItems.itemId == id) {
+        //            //console.log(id,'true');
+        //            return true;
+        //        } else {
+        //            //console.log(id,'false');
+        //            return false;
+        //        }
+        //    };
+        //    var items = allItems.filter(filterByID);
+        //    return items;
+        //};
+        factory.getCategoryName = function (id) {
+            var name = this.getCategoryData()[id - 1].title;
+            return name;
+        };
+        return factory
+    }]);
